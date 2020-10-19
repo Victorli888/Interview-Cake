79add33fcaf406dc39c18e5cae4202f710b94a5e
Victorli888 <victorli7462@gmail.com>
2020-10-19 11:56:49 -0700

    Test cases & method 3 added

--- AppleStocks.py
+++ AppleStocks.py
@@ -42,12 +42,7 @@ def get_max_profit_1(prices):
 
 # Solution 2:
 def get_max_profit_2(prices):
-    """Returns the maximum profit
 
-    >>> get_max_profit_3([10, 7, 5, 8, 11, 9])
-    6
-
-    """
     # instantiate variables to track min price, max profit
     min_price = prices[0]
     max_profit = 0
@@ -71,7 +66,54 @@ def get_max_profit_2(prices):
 # Runtime is O(n) b/c we go through the list only once.
 
 
+def get_max_profit_3(prices):
+    min_price = prices[0]
+    max_price = prices[1]
+
+    for price in prices:
+        min_price = min(price, min_price)
+        max_price = max(price, max_price)
+
+    profit = max_price - min_price
+    return profit
 
 
+# Tests
+import unittest
+class Test(unittest.TestCase):
+
+    def test_price_goes_up_then_down(self):
+        actual = get_max_profit_3([1, 5, 3, 2])
+        expected = 4
+        self.assertEqual(actual, expected)
+
+    def test_price_goes_down_then_up(self):
+        actual = get_max_profit_3([7, 2, 8, 9])
+        expected = 7
+        self.assertEqual(actual, expected)
+
+    def test_price_goes_up_all_day(self):
+        actual = get_max_profit_3([1, 6, 7, 9])
+        expected = 8
+        self.assertEqual(actual, expected)
+
+    def test_price_goes_down_all_day(self):
+        actual = get_max_profit_3([9, 7, 4, 1])
+        expected = -2
+        self.assertEqual(actual, expected)
+
+    def test_price_stays_the_same_all_day(self):
+        actual = get_max_profit_3([1, 1, 1, 1])
+        expected = 0
+        self.assertEqual(actual, expected)
+
+    def test_error_with_empty_prices(self):
+        with self.assertRaises(Exception):
+            get_max_profit_3([])
+
+    def test_error_with_one_price(self):
+        with self.assertRaises(Exception):
+            get_max_profit_3([1])
 
 
+unittest.main(verbosity=2)
