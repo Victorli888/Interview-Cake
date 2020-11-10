5847df5827c510a1cbdf03d2e4090b885cc0b798
Victorli888 <victorli7462@gmail.com>
2020-11-09 21:44:18 -0800

    O(n) space solution

--- FindRepeatSpace.py
+++ FindRepeatSpace.py
@@ -1,14 +1,35 @@
 """Write a function which finds an integer that appears more than once in our list.
  (If there are multiple duplicates, you only need to find one of them.)"""
 
+"""
+iterative method Iteriate all the way through the array
+"""
+def fr(arr):  # find repeat
+    # Create an array to verify repeated items
+    numbers_seen = []
+    # iteriate through and find repeats
+    for number in arr:
+        if number in numbers_seen:
+            print("repeat Found!")
+            return number
+
+        else: # No repeat found at this step so add it to seen and continue
+            numbers_seen.append(number)
+
+    # If the for loop finishes that means no repeats were found in the array
+    print("No Repeats found")
+    return None
+
+# Analysis Time: O(n) Space: O(n)  ~~~ Worst cases  we can optimize space!
+
 def find_repeat(nums):
     floor = 1
     ceiling = len(nums) -1
 
     while floor < ceiling:
         mid_pt = floor +((ceiling-floor)//2)
-        lower_range_floor, lower_range_ceiling = floor, mid_pt
-        upper_range_floor, upper_range_ceiling = mid_pt+1, ceiling
+        lower_range_floor, lower_range_ceiling = floor, mid_pt  # Lower Range
+        upper_range_floor, upper_range_ceiling = mid_pt+1, ceiling  # Upper Range
 
     # Count the number of items in the lower range
         items_in_lower_range = 0
@@ -18,7 +39,7 @@ def find_repeat(nums):
                 items_in_lower_range += 1
 
             distinct_possible_int_in_lower_range = (lower_range_ceiling - lower_range_floor + 1)
-
+            # Is it in the upper range?
             if items_in_lower_range > distinct_possible_int_in_lower_range:
                 # There must be a duplicate in the lower range, approach it iteratively
                 floor, ceiling = lower_range_floor, lower_range_ceiling
@@ -27,9 +48,9 @@ def find_repeat(nums):
                 # There must be a duplicate in the upper range, approach it iteratively
                 floor, ceiling = upper_range_floor, upper_range_ceiling
 
-
     return floor
 
+# Anaylsis Space: O(1) Time: O(nlog(n))
 
 
 # Tests
